d91d0081c8fed7a15d566306d3c074b65c3bbdd8
dhrruvchotai <23010101051@darshan.ac.in>
2024-08-09 21:15:37 +0530

    Changed for display time

--- Easy.js
+++ Easy.js
@@ -283,7 +283,7 @@ let icons;
     
     function displayTime(){ 
         let interval = 0;
-        let totalIntervals = 700;
+        let totalIntervals = 600;
         let increment = 100 / totalIntervals;
 
 
